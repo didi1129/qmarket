8b2761413a0a798fe5a4a824a330646a2af53974
leesangdal <asksa1256@gmail.com>
2025-12-05 15:10:43 +0900

    feat: faq 항목 추가 - 아이템 수량 관련

--- src/app/(global)/faq/page.tsx
+++ src/app/(global)/faq/page.tsx
@@ -19,52 +19,30 @@ export default function FAQPage() {
       <Accordion type="single" collapsible className="w-full">
         <AccordionItem value="faq-1" className="border-b">
           <AccordionTrigger className="text-lg font-semibold text-left hover:no-underline py-4 text-gray-700">
-            시세는 어떻게 계산되나요?
+            시세 조작 방지가 가능한가요?
           </AccordionTrigger>
-
           <AccordionContent className="text-gray-600 p-4 bg-gray-50 border-t">
-            <p className="leading-relaxed">
-              시세는 <b>다수의 거래 내역 데이터</b>를 기반으로 계산되며,
-              일반적인 평균값은 아닙니다.
-              <br />
-              또한 <b>비정상적인 거래 가격은 시세에 반영되지 않도록</b> 설계되어
-              있습니다.
-            </p>
+            <div className="leading-relaxed">
+              시세는 어디까지나 참고용이기 때문에, 필요하신 경우 '큐플레이
+              아카이브' 디스코드 채널 또는 별도 커뮤니티에서 추가로 조사해보시는
+              것을 권장드립니다.
+            </div>
           </AccordionContent>
         </AccordionItem>
 
         <AccordionItem value="faq-2" className="border-b">
           <AccordionTrigger className="text-lg font-semibold text-left hover:no-underline py-4 text-gray-700">
-            판매자가 직접 &apos;판매 완료&apos;로 수정하는데, 시세 조작 방지가
-            가능한가요?
+            구매/판매 시 동일한 아이템을 여러 개 등록하고 싶어요.
           </AccordionTrigger>
-
           <AccordionContent className="text-gray-600 p-4 bg-gray-50 border-t">
             <div className="leading-relaxed">
-              판매자가 실제로 거래하지 않고 낮은 가격이나 높은 가격으로 임의의
-              거래를 등록할 수는 있지만, 이는{" "}
-              <b>시세에 반영되지 않거나 매우 제한적인 영향</b>을 미칩니다.
+              - 동일 아이템을 2개 이상 구매하는 경우가 드물고,
               <br />
-              시세를 조작하려면 매우 많은 수의 허위 거래를 지속적으로 등록해야
-              하는데, 이 경우 로그에 거래 내역이 모두 남기 때문에{" "}
-              <div className="space-y-2 pl-4 border-l-2 mt-2 py-1">
-                <div className="flex items-start">
-                  <span className="text-sm font-bold mr-2">•</span>
-                  <p className="font-bold">계정이 제재되며,</p>
-                </div>
-
-                <div className="flex items-start">
-                  <span className="text-sm font-bold mr-2">•</span>
-                  <p className="font-bold">조작 내역은 삭제되고,</p>
-                </div>
-
-                <div className="flex items-start">
-                  <span className="text-sm font-bold mr-2">•</span>
-                  <p className="font-bold">
-                    아이템 시세는 원래대로 복원됩니다.
-                  </p>
-                </div>
-              </div>
+              - 동일 아이템을 한번에 여러 개 구매/판매 처리할 경우 시세 조작의
+              가능성도 있어서
+              <br /> 한 번에 1개의 아이템만 거래 완료 처리하도록 했습니다.
+              <br /> 만약 확성기처럼 최소 10개 이상의 아이템을 한번에 거래해야
+              할 경우, 거래 메시지에 관련 내용을 작성해주세요.
             </div>
           </AccordionContent>
         </AccordionItem>
