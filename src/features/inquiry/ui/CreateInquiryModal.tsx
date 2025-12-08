3e067dd3ac0e76b5e5513ca7f0ee29c0983fb940
leesangdal <asksa1256@gmail.com>
2025-12-08 14:20:00 +0900

    feat: 문의하기 - 문의 카테고리 추가, rhf+zod 연동

--- src/features/inquiry/ui/CreateInquiryModal.tsx
+++ src/features/inquiry/ui/CreateInquiryModal.tsx
@@ -16,60 +16,55 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/shared/ui/select";
+import { Label } from "@radix-ui/react-label";
 import { Textarea } from "@/shared/ui/textarea";
-import { ReactNode, useState, type FormEvent } from "react";
+import { ReactNode } from "react";
 import { toast } from "sonner";
 import { supabase } from "@/shared/api/supabase-client";
 import { Input } from "@/shared/ui/input";
 import { MailQuestionMark } from "lucide-react";
-import { useUser } from "@/shared/hooks/useUser";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { InquiryFormSchema, InquiryFormType } from "../model/inquirySchema";
+import { INQUIRY_CATEGORY } from "@/shared/config/constants";
 
 const CreateInquiryModal = ({ trigger }: { trigger?: ReactNode }) => {
-  const [contact, setContact] = useState("");
-  const [inquiry, setInquiry] = useState("");
-  const [inquiryCategory, setInquiryCategory] = useState("");
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const { data: user } = useUser();
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { isSubmitting, errors, isValid },
+    control,
+  } = useForm({
+    resolver: zodResolver(InquiryFormSchema),
+    defaultValues: {
+      inquiry_category: INQUIRY_CATEGORY[0],
+      message: "",
+      contact: "",
+    },
+  });
 
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-
+  const onSubmit = async (data: InquiryFormType) => {
     try {
-      if (inquiryCategory === "") {
-        toast.error("문의 유형을 선택해주세요.");
-        return;
-      }
-
-      if (inquiry === "") {
-        toast.error("문의사항을 입력해주세요.");
-        return;
-      }
-
       const { error } = await supabase.from("inquiry").insert([
         {
-          inquiry,
-          contact: user ? user.email : contact,
-          inquiry_category: inquiryCategory,
+          inquiry_category: data.inquiry_category,
+          message: data.message,
+          contact: data.contact ? data.contact : "",
         },
       ]);
 
       if (error) throw error;
 
       toast.success("문의가 등록되었습니다.");
-
-      setInquiry("");
-      setContact("");
-      setInquiryCategory("");
+      reset();
     } catch (error) {
       if (error instanceof Error) {
-        toast.error(`문의 등록 실패: ${error.message}`);
+        toast.error(`${error.message}`);
       } else {
-        toast.error("알 수 없는 오류가 발생했습니다.");
+        toast.error("문의 등록 실패: 알 수 없는 에러가 발생했습니다.");
       }
       console.error(error);
-    } finally {
-      setIsSubmitting(false);
     }
   };
 
@@ -86,35 +81,74 @@ const CreateInquiryModal = ({ trigger }: { trigger?: ReactNode }) => {
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader className="mb-4">
           <DialogTitle className="text-left">문의하기</DialogTitle>
-          <DialogDescription className="text-left">
-            {!user && "신고 기능은 로그인 후 이용할 수 있습니다."}
-          </DialogDescription>
+          <DialogDescription></DialogDescription>
         </DialogHeader>
 
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="grid gap-8">
-            <div className="grid gap-3">
-              <Textarea
-                id="inquiry"
-                name="inquiry"
-                placeholder="내용을 입력해주세요."
-                value={inquiry}
-                onChange={(e) => setInquiry(e.target.value)}
+            <div className="flex flex-col gap-2">
+              <Label className="text-sm">카테고리</Label>
+              <Controller
+                name="inquiry_category"
+                control={control}
+                render={({ field }) => (
+                  <Select
+                    value={field.value}
+                    onValueChange={(val) => field.onChange(val)}
+                  >
+                    <SelectTrigger className="w-[150px]">
+                      <SelectValue />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {INQUIRY_CATEGORY.map((ctg) => (
+                        <SelectItem key={ctg} value={ctg}>
+                          {ctg}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                )}
               />
+
+              {errors.inquiry_category && (
+                <p className="text-red-600 text-sm mt-1">
+                  {errors.inquiry_category.message}
+                </p>
+              )}
             </div>
 
-            <div className="grid gap-3">
-              <label htmlFor="contact" className="text-sm">
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="message" className="text-sm">
+                내용
+              </Label>
+              <Textarea
+                id="message"
+                placeholder="내용을 입력해주세요."
+                className="resize-none min-h-24"
+                {...register("message")}
+              />
+              {errors.message && (
+                <p className="text-red-600 text-sm mt-1">
+                  {errors.message.message}
+                </p>
+              )}
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="contact" className="text-sm">
                 연락처 <span className="text-gray-400">(선택)</span>
-              </label>
+              </Label>
               <Input
                 id="contact"
-                name="contact"
                 type="email"
                 placeholder="이메일"
-                value={contact}
-                onChange={(e) => setContact(e.target.value)}
+                {...register("contact")}
               />
+              {errors.contact && (
+                <p className="text-red-600 text-sm mt-1">
+                  {errors.contact.message}
+                </p>
+              )}
             </div>
           </div>
 
@@ -124,7 +158,9 @@ const CreateInquiryModal = ({ trigger }: { trigger?: ReactNode }) => {
                 닫기
               </Button>
             </DialogClose>
-            <Button>{isSubmitting ? "등록 중..." : "등록하기"}</Button>
+            <Button disabled={isSubmitting || !isValid}>
+              {isSubmitting ? "등록 중..." : "등록하기"}
+            </Button>
           </DialogFooter>
         </form>
       </DialogContent>
