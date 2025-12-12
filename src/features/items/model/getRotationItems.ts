e75fea5e2276a08592ffe839d02b9b7e2e254389
leesangdal <asksa1256@gmail.com>
2025-12-12 13:04:25 +0900

    feat: 로테이션 아이템 캐싱 방식 no-store로 변경

--- src/features/items/model/getRotationItems.ts
+++ src/features/items/model/getRotationItems.ts
@@ -46,7 +46,7 @@ const getRotationItems = async ({
         apikey: ANON_KEY,
         Authorization: `Bearer ${ANON_KEY}`,
       },
-      cache: "force-cache", // 정적 렌더링(ssg), db에서 rotation_date 업데이트 해주고 빌드+push
+      cache: "no-store", // 빌드 시 변경
     });
 
     if (!response.ok) {
